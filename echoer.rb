681d34c4972287468fe8cee9fffe45e4c05791a0
haleygirl <haley.yoder@github.com>
2014-06-17 20:04:42 -0700

    first commit

---
+++ echoer.rb
@@ -0,0 +1,3 @@
+print "what is your name?"
+name = gets
+print " Hello" + name
---
+++ practice.rb
@@ -0,0 +1,3 @@
+puts 'this code is in the file!'
+some_variable = 19
+puts "I stored a variable with the value #{some_variable}!"
